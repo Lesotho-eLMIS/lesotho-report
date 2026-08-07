f67b5f9af71e6b7e21a74479ca8c55900769b2da
lethusang <kotjomelalethusang@gmail.com>
2026-08-07 03:59:41 +0200

    delete old product loss report

---
+++ src/main/resources/db/migration/20260807031600000__delete_product_loss.sql
@@ -0,0 +1,2 @@
+DELETE FROM report.template_parameters where templateid = '3bbab4ff-9c49-42c8-8aa5-4534f09ee542';
+DELETE FROM report.jasper_templates where id = '3bbab4ff-9c49-42c8-8aa5-4534f09ee542';
